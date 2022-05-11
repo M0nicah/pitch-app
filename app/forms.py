f931ddec046ef384db3b49b91801489cf738c358
M0nicah <monicawughanga@gmail.com>
2022-05-11 14:31:17 +0300

    add validation for usernames for login funcs

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from click import confirm
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
+from app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
@@ -11,16 +11,13 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit_form = SubmitField('Sign Up')
 
-        
-def validate_email(self, email):
-        if User.query.filter_by(email=email.data).first():
-            raise ValidationError("Email already registered!")
-
-def validate_uname(self, uname):
-    if User.query.filter_by(username=username.data).first():
-        raise ValidationError("Username already taken!")
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit_form = SubmitField('Log in')
+
+
+def validate_username(self, username):
+    if User.query.filter_by(username=username.data).first():
+        raise ValidationError("Username already taken!")
