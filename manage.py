5dd1d95cbb68d8d445be68121486f874e3e73c88
M0nicah <monicawughanga@gmail.com>
2022-05-11 23:32:16 +0300

    add test_user function to test user class

--- manage.py
+++ manage.py
@@ -4,6 +4,13 @@ from app.models import User, Pitch
 # Creating app instance
 app = createapp()
 
+@app.command
+def test():
+    """Run the unit tests"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 @app.shell_context_processor
 def make_shell_context():
